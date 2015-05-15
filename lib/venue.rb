bae2269454fa38cb7ced452abc7018b008edea34
drabelpdx <drabelpdx@gmail.com>
2015-05-15 10:52:00 -0700

    Adds callback on Venue class to titlecase name.

--- lib/venue.rb
+++ lib/venue.rb
@@ -2,5 +2,11 @@ class Venue < ActiveRecord::Base
   has_and_belongs_to_many(:bands)
   validates(:name, {:presence => true})
 
+  before_save(:titlecase_name)
 
+  private
+
+  define_method(:titlecase_name) do
+    self.name=(name().titlecase())
+  end
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -1,9 +1,13 @@
 require('spec_helper')
 
 describe(Venue) do
-  it { should have_and_belong_to_many(:bands)}
-end
 
-describe(Venue) do
+  it { should have_and_belong_to_many(:bands)}
+
   it { should validate_presence_of(:name) }
+
+  it("converts the name to titlecase") do
+    venue = Venue.create({:name => "harry's wild pit"})
+    expect(venue.name()).to(eq("Harry's Wild Pit"))
+  end
 end
