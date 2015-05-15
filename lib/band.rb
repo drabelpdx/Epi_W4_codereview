35aee5061081da8b44c389bcc206aaab68645714
drabelpdx <drabelpdx@gmail.com>
2015-05-15 10:48:53 -0700

    Adds callback on Band class to titlecase name.

--- lib/band.rb
+++ lib/band.rb
@@ -2,5 +2,11 @@ class Band <ActiveRecord::Base
   has_and_belongs_to_many(:venues)
   validates(:name, {:presence => true})
 
+  before_save(:titlecase_name)
 
+  private
+
+  define_method(:titlecase_name) do
+    self.name=(name().titlecase())
+  end
 end
--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -1,9 +1,14 @@
 require('spec_helper')
 
 describe(Band) do
-  it { should have_and_belong_to_many(:venues)}
-end
 
-describe(Band) do
+  it { should have_and_belong_to_many(:venues)}
+
   it { should validate_presence_of(:name) }
+
+  it("converts the name to titlecase") do
+    band = Band.create({:name => "fine bunch of boys"})
+    expect(band.name()).to(eq("Fine Bunch Of Boys"))
+  end
+
 end
